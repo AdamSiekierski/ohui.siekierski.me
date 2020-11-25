2324fb318560e5576d1cc291f879012ae37440b2
Adam Siekierski <a@siekierski.ml>
2020-11-25 11:29:09 +0100

    Use redirect instead of sending file

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import fs from 'fs';
-import path from 'path';
 
 const IMAGES = ['ohui1.jpg', 'ohui2.jpg'];
 
 export default function handler(req: NowRequest, res: NowResponse) {
-  const file = path.join(__dirname, '../images', IMAGES[Math.floor(Math.random() * 2)]);
-
-  const stat = fs.statSync(file);
-
-  try {
-    res.writeHead(200, { 'Content-Type': 'image/jpeg', 'Content-Length': stat.size });
-
-    fs.createReadStream(file).pipe(res);
-  } catch {
-    res.status(500).json({ message: `Internal server error` });
-  }
+  res.redirect(`https://ohui.siekierski.ml/images/${IMAGES[Math.floor(Math.random() * 2)]}`);
 }
