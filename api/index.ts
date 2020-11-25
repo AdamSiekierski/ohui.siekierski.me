526dd7dd5e9609684e692c97de1c68a0b0491df2
Adam Siekierski <a@siekierski.ml>
2020-11-25 11:10:56 +0100

    Fix static files

--- api/index.ts
+++ api/index.ts
@@ -5,11 +5,15 @@ import path from 'path';
 const IMAGES = ['ohui1.jpg', 'ohui2.jpg'];
 
 export default function handler(req: NowRequest, res: NowResponse) {
-  const file = path.join(__dirname, IMAGES[Math.floor(Math.random() * 2)]);
+  const file = path.join(__dirname, '../images', IMAGES[Math.floor(Math.random() * 2)]);
 
   const stat = fs.statSync(file);
 
-  res.writeHead(200, { 'Content-Type': 'image/jpeg', 'Content-Length': stat.size });
+  try {
+    res.writeHead(200, { 'Content-Type': 'image/jpeg', 'Content-Length': stat.size });
 
-  fs.createReadStream(file).pipe(res);
+    fs.createReadStream(file).pipe(res);
+  } catch {
+    res.status(500).json({ message: `Internal server error` });
+  }
 }
