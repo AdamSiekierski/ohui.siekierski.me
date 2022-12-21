d4a6f7bb29f56d5623b2b19bd58f79fe2e59910f
Adam Siekierski <adam.siekiera@outlook.com>
2022-12-21 22:14:17 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,5 @@ import { NowRequest, NowResponse } from '@vercel/node';
 const IMAGES = ['ohui1.jpg', 'ohui2.jpg'];
 
 export default function handler(req: NowRequest, res: NowResponse) {
-  res.redirect(`https://ohui.siekierski.ml/images/${IMAGES[Math.floor(Math.random() * 2)]}`);
+  res.redirect(`/images/${IMAGES[Math.floor(Math.random() * 2)]}`);
 }
